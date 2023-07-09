2f7c2a90a53efa7a6fe143078822a997644ba862
SAGAR KUMAR JHA <jha8768@gmail.com>
2023-07-09 17:08:36 +0530

    Search page fixes

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -12,55 +12,53 @@ import Card from './Card';
 
 const SearchPage = () => {
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{
-        backgroundColor: '#EEE',
-        marginTop: 40,
+    <View
+      style={{
+        backgroundColor: '#F9F9F9',
       }}>
-      <View
+      <FlatList
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        data={Array.from({length: 8}).map((_, idx) => idx + 1)}
+        renderItem={({item, index}) => (
+          <View style={{marginTop: 50, marginBottom: 10}}>
+            <Card height={250} width={185} />
+          </View>
+        )}
+        ListHeaderComponent={<SearchBar />}
+      />
+    </View>
+  );
+};
+
+const SearchBar = () => {
+  return (
+    <View
+      style={{
+        marginLeft: 40,
+        flexDirection: 'row',
+        marginTop: 30,
+        width: '85%',
+        borderRadius: 30,
+        alignItems: 'center',
+        backgroundColor: '#F9F9F9',
+        height: 50,
+        borderColor: 'red',
+        borderWidth: 1,
+      }}>
+      <Image
+        source={require('../images/search.png')}
         style={{
-          marginLeft: 40,
-          flexDirection: 'row',
-          marginTop: 10,
-          width: '85%',
-          borderRadius: 30,
-          alignItems: 'center',
-          backgroundColor: '#F9F9F9',
-          height: 50,
-        }}>
-        <Image
-          source={require('../images/search.png')}
-          style={{
-            height: 16,
-            width: 16,
-            resizeMode: 'contain',
-            tintColor: '#000',
-            marginRight: 15,
-            marginLeft: 20,
-          }}
-        />
-        <TextInput placeholder="Search Product" placeholderTextColor={'#000'} />
-      </View>
-      <View
-        style={{
-          marginTop: 20,
-          backgroundColor: '#F9F9F9',
-          borderRadius: 30,
-          paddingTop: 20,
-          paddingBottom: 80,
-        }}>
-        <FlatList
-          numColumns={2}
-          data={Array.from({length: 8}).map((_, idx) => idx + 1)}
-          renderItem={({item, index}) => (
-            <View style={{marginTop: 50, marginBottom: 10}}>
-              <Card height={250} width={185} />
-            </View>
-          )}
-        />
-      </View>
-    </ScrollView>
+          height: 16,
+          width: 16,
+          resizeMode: 'contain',
+          tintColor: '#000',
+          marginRight: 15,
+          marginLeft: 20,
+        }}
+      />
+      <TextInput placeholder="Search Product" placeholderTextColor={'#000'} />
+    </View>
   );
 };
 
